bf7a63fc8ae55e6b6d524161a73e82621d5ebb1d
Yamileth Londoño Burgos <142650446+Yamis2706@users.noreply.github.com>
2026-05-11 19:40:23 -0500

    Creación de arquitectura atómica y componentes

---
+++ src/app/atoms/nav-section-label/nav-section-label.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-section-label',
+  imports: [],
+  templateUrl: './nav-section-label.html',
+  styleUrl: './nav-section-label.css',
+})
+export class NavSectionLabel {
+
+}
---
+++ src/app/atoms/page-title/page-title.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-title',
+  imports: [],
+  templateUrl: './page-title.html',
+  styleUrl: './page-title.css',
+})
+export class PageTitle {
+
+}
